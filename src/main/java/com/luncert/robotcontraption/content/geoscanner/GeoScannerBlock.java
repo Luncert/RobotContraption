6bc0993136e2846037c173704fe3f7218ae76e0a
Luncert <2725115515@qq.com>
2022-08-09 17:05:22 +0800

    - aircraft station item should have entity id - vacuum_pump - can't mine block

--- src/main/java/com/luncert/robotcontraption/content/geoscanner/GeoScannerBlock.java
+++ src/main/java/com/luncert/robotcontraption/content/geoscanner/GeoScannerBlock.java
@@ -2,15 +2,14 @@ package com.luncert.robotcontraption.content.geoscanner;
 
 import com.luncert.robotcontraption.index.RCTileEntities;
 import com.simibubi.create.foundation.block.ITE;
-import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
-public class GeoScannerBlock extends DirectionalBlock implements ITE<GeoScannerTileEntity> {
+public class GeoScannerBlock extends HorizontalDirectionalBlock implements ITE<GeoScannerTileEntity> {
 
     public GeoScannerBlock(Properties p_49795_) {
         super(p_49795_);
     }
-
 
     @Override
     public Class<GeoScannerTileEntity> getTileEntityClass() {
--- src/main/java/com/luncert/robotcontraption/content/storageaccessor/StorageAccessorBlock.java
+++ src/main/java/com/luncert/robotcontraption/content/storageaccessor/StorageAccessorBlock.java
@@ -2,10 +2,10 @@ package com.luncert.robotcontraption.content.storageaccessor;
 
 import com.luncert.robotcontraption.index.RCTileEntities;
 import com.simibubi.create.foundation.block.ITE;
-import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
-public class StorageAccessorBlock extends DirectionalBlock implements ITE<StorageAccessorTileEntity> {
+public class StorageAccessorBlock extends HorizontalDirectionalBlock implements ITE<StorageAccessorTileEntity> {
 
     public StorageAccessorBlock(Properties p_52591_) {
         super(p_52591_);
